d82ac560de6fee601f69032883bd695bfe14e351
N1K340 <77387202+N1K340@users.noreply.github.com>
2021-01-21 19:48:39 +1000

    Update 767CrewPack.lua

--- Scripts/767CrewPack.lua
+++ Scripts/767CrewPack.lua
@@ -753,7 +753,7 @@ if PLANE_ICAO == "B752" or PLANE_ICAO == "B753" or PLANE_ICAO == "B762" or PLANE
                 gsPlayed = false
             end
             -- Loc Capture Left of localiser (CDI Right)
-            if WEIGHT_ON_WHEELS == 0 and LOC_RECEIVED == 1 and LOC_DEVIATION > -1.95 and LOC_DEVIATION <= 0 and not locPlayed and not togaEvent and not toCalloutMode
+            if WEIGHT_ON_WHEELS == 0 and LOC_RECEIVED == 1 and LOC_DEVIATION < 1.95 and LOC_DEVIATION <= 0 and not locPlayed and not togaEvent and not toCalloutMode
             then
                 play_sound(LOCcap_snd)
                 print("767CrewPack: LOC Active")
